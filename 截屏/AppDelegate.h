83e7b54211b4e5b70a1db360dc1518ceb8e24297
PYC/Mr.ma <pyc@MRmadeMac-mini.local>
2014-07-07 14:57:04 +0800

    Initial Commit

---
+++ 截屏/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  截屏
+//
+//  Created by PYC/Mr.ma on 14-7-7.
+//  Copyright (c) 2014年 PYC\MFW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 截屏/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  截屏
+//
+//  Created by PYC/Mr.ma on 14-7-7.
+//  Copyright (c) 2014年 PYC\MFW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
